72da8680700fbd8f61d177faa4bb0573485fd98f
Tibor Barta <tiborbarta87@gmail.com>
2022-07-12 00:55:15 +0200

    build before push

--- dist/aw-components.es.js
+++ dist/aw-components.es.js
@@ -54,6 +54,133 @@ class AlpDropdownEl extends HTMLElement {
     });
   }
 }
+var colorpickerTemplate = `<template id="alp-colorpicker-template">
+    <div x-data="colorpicker">
+        <label for="red">Red</label>
+        <input id="red" type="number" min="0" max="255" x-model="red" placeholder="red">
+        <input type="range" min="0" max="255" x-model="red">
+        <br>
+        <label for="green">Green</label>
+        <input id="green" type="number" min="0" max="255" x-model="green" placeholder="green">
+        <input type="range" min="0" max="255" x-model="green">
+        <br>
+        <label for="blue">Blue</label>
+        <input id="blue" type="number" min="0" max="255" x-model="blue" placeholder="blue">
+        <input type="range" min="0" max="255" x-model="blue">
+        <br>
+        <br>
+        <div :style="{ width: '50px', height: '50px',backgroundColor:'rgb('+red+','+green+','+blue+')'}"></div>
+        <br>
+        <br>
+        <button @click="fillCanvas()">Fill canvas with random colors</button>
+        <br>
+        <br>
+        <span>Click on canvas to select color</span>
+        <br>
+        <br>
+        <canvas width="300px" height="300px"></canvas>
+
+
+    </div>
+</template>
+`;
+class AlpColorpickerEl extends HTMLElement {
+  constructor() {
+    super();
+    __publicField(this, "modulename", "colorpicker");
+    __publicField(this, "moduledata", {
+      init() {
+        let _this = this;
+        this.canvasEl = this.$el.querySelector("canvas");
+        function getElementPosition(obj) {
+          var curleft = 0, curtop = 0;
+          if (obj.offsetParent) {
+            do {
+              curleft += obj.offsetLeft;
+              curtop += obj.offsetTop;
+            } while (obj = obj.offsetParent);
+            return { x: curleft, y: curtop };
+          }
+          return void 0;
+        }
+        function getEventLocation(element, event) {
+          var pos = getElementPosition(element);
+          return {
+            x: event.pageX - pos.x,
+            y: event.pageY - pos.y
+          };
+        }
+        this.canvasEl.addEventListener("click", function(event) {
+          var eventLocation = getEventLocation(this, event);
+          var context = this.getContext("2d");
+          var pixelData = context.getImageData(eventLocation.x, eventLocation.y, 1, 1).data;
+          _this.red = pixelData[0];
+          _this.green = pixelData[1];
+          _this.blue = pixelData[2];
+        });
+        this.fillCanvas();
+      },
+      red: 0,
+      green: 0,
+      blue: 0,
+      available_colors: [],
+      canvasEl: false,
+      fillCanvas() {
+        const RGBToHSL = (r, g, b) => {
+          r /= 255;
+          g /= 255;
+          b /= 255;
+          const l = Math.max(r, g, b);
+          const s = l - Math.min(r, g, b);
+          const h = s ? l === r ? (g - b) / s : l === g ? 2 + (b - r) / s : 4 + (r - g) / s : 0;
+          return [
+            60 * h < 0 ? 60 * h + 360 : 60 * h,
+            100 * (s ? l <= 0.5 ? s / (2 * l - s) : s / (2 - (2 * l - s)) : 0),
+            100 * (2 * l - s) / 2
+          ];
+        };
+        const HSLToRGB = (h, s, l) => {
+          s /= 100;
+          l /= 100;
+          const k = (n) => (n + h / 30) % 12;
+          const a = s * Math.min(l, 1 - l);
+          const f = (n) => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+          return [255 * f(0), 255 * f(8), 255 * f(4)];
+        };
+        let colors = [];
+        for (let x = 0; x < 30; x++) {
+          for (let y = 0; y < 30; y++) {
+            let red = Math.round(Math.random() * (255 - 0));
+            let green = Math.round(Math.random() * (255 - 0));
+            let blue = Math.round(Math.random() * (255 - 0));
+            colors.push(RGBToHSL(red, green, blue));
+          }
+        }
+        colors.sort((a, b) => {
+          return Math.round(a[0] * 100) / 100 - Math.round(b[0] * 100) / 100 || Math.round(a[2] * 100) / 100 - Math.round(b[2] * 100) / 100;
+        });
+        console.log(colors);
+        const ctx = this.canvasEl.getContext("2d");
+        colors.map((color, index) => {
+          let rgb = HSLToRGB(color[0], color[1], color[2]);
+          ctx.fillStyle = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+          ctx.fillRect(index % 30 * 10, Math.floor(index / 30) * 10, 10, 10);
+        });
+      }
+    });
+    this.attachShadow({ mode: "open" });
+    const tmpEl = document.createElement("div");
+    tmpEl.insertAdjacentHTML("beforeend", colorpickerTemplate);
+    const elTemplate = tmpEl.querySelector("template");
+    this.shadowRoot.appendChild(elTemplate.content.cloneNode(true));
+  }
+  connectedCallback() {
+    document.addEventListener("alpine:initialized", () => {
+      Alpine.data(this.modulename, () => this.moduledata);
+      Alpine.initTree(this.shadowRoot);
+    });
+  }
+}
 if (typeof Alpine == "undefined") {
   let alpineScript = document.createElement("script");
   alpineScript.setAttribute("defer", "");
@@ -62,3 +189,4 @@ if (typeof Alpine == "undefined") {
 }
 customElements.define("alp-counter", AlpCounterEl);
 customElements.define("alp-dropdown", AlpDropdownEl);
+customElements.define("alp-colorpicker", AlpColorpickerEl);
--- dist/aw-components.umd.js
+++ dist/aw-components.umd.js
@@ -1,10 +1,10 @@
-(function(e){typeof define=="function"&&define.amd?define(e):e()})(function(){"use strict";var c=Object.defineProperty;var r=(e,t,n)=>t in e?c(e,t,{enumerable:!0,configurable:!0,writable:!0,value:n}):e[t]=n;var a=(e,t,n)=>(r(e,typeof t!="symbol"?t+"":t,n),n);var e="",t=`<template id="alp-counter-template">
+(function(r){typeof define=="function"&&define.amd?define(r):r()})(function(){"use strict";var E=Object.defineProperty;var M=(r,i,s)=>i in r?E(r,i,{enumerable:!0,configurable:!0,writable:!0,value:s}):r[i]=s;var m=(r,i,s)=>(M(r,typeof i!="symbol"?i+"":i,s),s);var r="",i=`<template id="alp-counter-template">
     <div x-data="counter">
         <span x-text="count"></span>
         <button @click="count+=step">Increment</button>
     </div>
 </template>
-`;class n extends HTMLElement{constructor(){super();a(this,"modulename","counter");a(this,"moduledata",{count:0,step:5});this.attachShadow({mode:"open"});const d=document.createElement("div");d.insertAdjacentHTML("beforeend",t);const l=d.querySelector("template");this.shadowRoot.appendChild(l.content.cloneNode(!0))}connectedCallback(){document.addEventListener("alpine:initialized",()=>{Alpine.data(this.modulename,()=>this.moduledata),Alpine.initTree(this.shadowRoot)})}}var i=`<template id="alp-dropdown-template">
+`;class s extends HTMLElement{constructor(){super();m(this,"modulename","counter");m(this,"moduledata",{count:0,step:5});this.attachShadow({mode:"open"});const o=document.createElement("div");o.insertAdjacentHTML("beforeend",i);const c=o.querySelector("template");this.shadowRoot.appendChild(c.content.cloneNode(!0))}connectedCallback(){document.addEventListener("alpine:initialized",()=>{Alpine.data(this.modulename,()=>this.moduledata),Alpine.initTree(this.shadowRoot)})}}var b=`<template id="alp-dropdown-template">
     <div class="dropdown-wrapper"  x-data="dropdown">
         <div class="dropdown-toggler" @click="open = !open">
             <slot name="dropdown-toggler"></slot>
@@ -14,4 +14,33 @@
         </div>
     </div>
 </template>
-`;class p extends HTMLElement{constructor(){super();a(this,"modulename","dropdown");a(this,"moduledata",{open:!1,toggle(){this.open=!this.open},close(){this.open=!1}});this.attachShadow({mode:"open"});const d=document.createElement("div");d.insertAdjacentHTML("beforeend",i);const l=d.querySelector("template");this.shadowRoot.appendChild(l.content.cloneNode(!0))}connectedCallback(){document.addEventListener("alpine:initialized",()=>{Alpine.data(this.modulename,()=>this.moduledata),Alpine.initTree(this.shadowRoot)})}}if(typeof Alpine=="undefined"){let o=document.createElement("script");o.setAttribute("defer",""),o.setAttribute("src","//unpkg.com/alpinejs"),document.head.appendChild(o)}customElements.define("alp-counter",n),customElements.define("alp-dropdown",p)});
+`;class x extends HTMLElement{constructor(){super();m(this,"modulename","dropdown");m(this,"moduledata",{open:!1,toggle(){this.open=!this.open},close(){this.open=!1}});this.attachShadow({mode:"open"});const o=document.createElement("div");o.insertAdjacentHTML("beforeend",b);const c=o.querySelector("template");this.shadowRoot.appendChild(c.content.cloneNode(!0))}connectedCallback(){document.addEventListener("alpine:initialized",()=>{Alpine.data(this.modulename,()=>this.moduledata),Alpine.initTree(this.shadowRoot)})}}var g=`<template id="alp-colorpicker-template">
+    <div x-data="colorpicker">
+        <label for="red">Red</label>
+        <input id="red" type="number" min="0" max="255" x-model="red" placeholder="red">
+        <input type="range" min="0" max="255" x-model="red">
+        <br>
+        <label for="green">Green</label>
+        <input id="green" type="number" min="0" max="255" x-model="green" placeholder="green">
+        <input type="range" min="0" max="255" x-model="green">
+        <br>
+        <label for="blue">Blue</label>
+        <input id="blue" type="number" min="0" max="255" x-model="blue" placeholder="blue">
+        <input type="range" min="0" max="255" x-model="blue">
+        <br>
+        <br>
+        <div :style="{ width: '50px', height: '50px',backgroundColor:'rgb('+red+','+green+','+blue+')'}"></div>
+        <br>
+        <br>
+        <button @click="fillCanvas()">Fill canvas with random colors</button>
+        <br>
+        <br>
+        <span>Click on canvas to select color</span>
+        <br>
+        <br>
+        <canvas width="300px" height="300px"></canvas>
+
+
+    </div>
+</template>
+`;class w extends HTMLElement{constructor(){super();m(this,"modulename","colorpicker");m(this,"moduledata",{init(){let o=this;this.canvasEl=this.$el.querySelector("canvas");function c(l){var e=0,t=0;if(l.offsetParent){do e+=l.offsetLeft,t+=l.offsetTop;while(l=l.offsetParent);return{x:e,y:t}}}function h(l,e){var t=c(l);return{x:e.pageX-t.x,y:e.pageY-t.y}}this.canvasEl.addEventListener("click",function(l){var e=h(this,l),t=this.getContext("2d"),n=t.getImageData(e.x,e.y,1,1).data;o.red=n[0],o.green=n[1],o.blue=n[2]}),this.fillCanvas()},red:0,green:0,blue:0,available_colors:[],canvasEl:!1,fillCanvas(){const o=(e,t,n)=>{e/=255,t/=255,n/=255;const d=Math.max(e,t,n),a=d-Math.min(e,t,n),p=a?d===e?(t-n)/a:d===t?2+(n-e)/a:4+(e-t)/a:0;return[60*p<0?60*p+360:60*p,100*(a?d<=.5?a/(2*d-a):a/(2-(2*d-a)):0),100*(2*d-a)/2]},c=(e,t,n)=>{t/=100,n/=100;const d=f=>(f+e/30)%12,a=t*Math.min(n,1-n),p=f=>n-a*Math.max(-1,Math.min(d(f)-3,Math.min(9-d(f),1)));return[255*p(0),255*p(8),255*p(4)]};let h=[];for(let e=0;e<30;e++)for(let t=0;t<30;t++){let n=Math.round(Math.random()*255),d=Math.round(Math.random()*(255-0)),a=Math.round(Math.random()*(255-0));h.push(o(n,d,a))}h.sort((e,t)=>Math.round(e[0]*100)/100-Math.round(t[0]*100)/100||Math.round(e[2]*100)/100-Math.round(t[2]*100)/100),console.log(h);const l=this.canvasEl.getContext("2d");h.map((e,t)=>{let n=c(e[0],e[1],e[2]);l.fillStyle=`rgb(${n[0]},${n[1]},${n[2]})`,l.fillRect(t%30*10,Math.floor(t/30)*10,10,10)})}});this.attachShadow({mode:"open"});const o=document.createElement("div");o.insertAdjacentHTML("beforeend",g);const c=o.querySelector("template");this.shadowRoot.appendChild(c.content.cloneNode(!0))}connectedCallback(){document.addEventListener("alpine:initialized",()=>{Alpine.data(this.modulename,()=>this.moduledata),Alpine.initTree(this.shadowRoot)})}}if(typeof Alpine=="undefined"){let u=document.createElement("script");u.setAttribute("defer",""),u.setAttribute("src","//unpkg.com/alpinejs"),document.head.appendChild(u)}customElements.define("alp-counter",s),customElements.define("alp-dropdown",x),customElements.define("alp-colorpicker",w)});
